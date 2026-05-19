a627f625e88a10d32e8a4034cd021b7e1fe6f0ad
pHsl-z <pHsl-z@users.noreply.github.com>
2026-05-20 01:03:01 +0800

    feat: 添加原生GGB模式、反推模式、下拉模式切换、显示题目按钮、窗口自适应、供应商更新、修复命令模式等

--- server.js
+++ server.js
@@ -1,42 +1,36 @@
-var http = require("http");
-var fs = require("fs");
-var path = require("path");
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
-var PORT = 8080;
-var DIR = __dirname;
+var dir = __dirname;
+var port = 8889;
 
-var MIME = {
-  ".html": "text/html",
-  ".css": "text/css",
-  ".js": "application/javascript",
-  ".mjs": "application/javascript",
-  ".json": "application/json",
-  ".png": "image/png",
-  ".jpg": "image/jpeg",
-  ".gif": "image/gif",
-  ".svg": "image/svg+xml",
-  ".ico": "image/x-icon",
-  ".wasm": "application/wasm",
-  ".woff": "font/woff",
-  ".woff2": "font/woff2",
-  ".ttf": "font/ttf",
-  ".otf": "font/otf",
-  ".mp3": "audio/mpeg",
-  ".ogg": "audio/ogg",
-  ".wav": "audio/wav",
-  ".xml": "text/xml",
-  ".txt": "text/plain",
+var mimeTypes = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.gif': 'image/gif',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon'
 };
 
-http.createServer(function (req, res) {
-  var url = req.url.split("?")[0];
-  if (url === "/") url = "/index.html";
-  var fp = path.join(DIR, url);
-  if (!fs.existsSync(fp)) { res.writeHead(404); res.end("Not Found"); return; }
-  var ext = path.extname(fp).toLowerCase();
-  var ct = MIME[ext] || "application/octet-stream";
-  res.writeHead(200, { "Content-Type": ct + "; charset=utf-8" });
-  fs.createReadStream(fp).pipe(res);
-}).listen(PORT, function () {
-  console.log("Server running at http://localhost:" + PORT);
+http.createServer(function(req, res) {
+  var filePath = path.join(dir, req.url === '/' ? 'index.html' : req.url);
+  var ext = path.extname(filePath);
+  var contentType = mimeTypes[ext] || 'application/octet-stream';
+
+  fs.readFile(filePath, function(err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not Found');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType + '; charset=utf-8' });
+      res.end(data);
+    }
+  });
+}).listen(port, function() {
+  console.log('Server running at http://127.0.0.1:' + port + '/');
 });
